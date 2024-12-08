9a6b065816ac936221fe2c6f9dba59e0bd426a25
joao5142 <joaopauloneto3687@gmail.com>
2024-12-08 00:45:54 -0300

    feat: add empty pages

---
+++ apps/frontend/src/pages/bills/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="d-flex align-center">bills</div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: 'default-view-layout',
+})
+</script>
+
+<style scoped></style>
---
+++ apps/frontend/src/pages/budget/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="d-flex align-center">budget</div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: 'default-view-layout',
+})
+</script>
+
+<style scoped></style>
