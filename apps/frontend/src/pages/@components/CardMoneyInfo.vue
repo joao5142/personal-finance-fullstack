3ab82e294c35867308b063a93f1fdfbfaf2ba5bd
joao5142 <joaopauloneto3687@gmail.com>
2024-12-08 00:45:14 -0300

    feat: add components

---
+++ apps/frontend/src/pages/@components/CardMoneyInfo.vue
@@ -0,0 +1,36 @@
+<template>
+  <div
+    :class="
+      cn('flex flex-col justify-center pl-app-300 detail', {
+        [`before:bg-app-${props.item.theme}`]: true,
+      })
+    "
+  >
+    <Text as="span" size="preset5" class="block text-app-grey-500">{{ item.label }}</Text>
+    <Text as="span" size="preset4" class="text-app-grey-900" weight="700">{{ item.value }}</Text>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Text from '@/components/ui/text/Text.vue'
+import { cn } from '@/lib/utils'
+import { ColorTypes } from '@/theme/colors'
+
+interface IProps {
+  item: {
+    label: string
+    value: number
+    theme: ColorTypes
+  }
+}
+
+const props = defineProps<IProps>()
+
+const classes = computed(() => {
+  return {
+    [`before:bg-app-${props.item.theme}`]: true,
+  }
+})
+</script>
+
+<style scoped></style>
